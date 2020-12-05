c437e4ef1c6bb9abc5c3908e83d3625d36dea1b3
Leonid Khvatskin <leonkhvatskin@gmail.com>
2020-12-05 03:41:05 +0300

    via coroutines

--- app/src/main/java/com/example/imgdownload/MainActivity.kt
+++ app/src/main/java/com/example/imgdownload/MainActivity.kt
@@ -2,62 +2,44 @@ package com.example.imgdownload
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
-import java.lang.Exception
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var loadImg: ImageView
-    private lateinit var loadBtn: Button
-    private lateinit var spinner: ProgressBar
+    private var bitmap: Bitmap? = null
+    private val imgURL = "https://im0-tub-ru.yandex.net/i?id=73b3f62833333e8a1817a3106ac1ef31&n=13"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        loadImg = findViewById(R.id.load_img)
-        loadBtn = findViewById(R.id.load_btn)
-        spinner = findViewById(R.id.progress_bar)
+        val loadImg = findViewById<ImageView>(R.id.load_img)
+        val spinner = findViewById<ProgressBar>(R.id.progress_bar)
+        val loadBtn = findViewById<Button>(R.id.load_btn)
 
         loadBtn.setOnClickListener {
-            val task = ImgLoadTask()
-            task.execute("https://im0-tub-ru.yandex.net/i?id=73b3f62833333e8a1817a3106ac1ef31&n=13")
-        }
-    }
-
-    inner class ImgLoadTask: AsyncTask<String, Void, Bitmap>() {
-        override fun onPreExecute() {
-            super.onPreExecute()
-            loadBtn.visibility = View.INVISIBLE
-            spinner.visibility = View.VISIBLE
-        }
-
-        override fun doInBackground(vararg params: String?): Bitmap? {
-            val url = params[0]!!
-            val resBitmap: Bitmap?
-            try {
-                val inpStream = java.net.URL(url).openStream()
-                resBitmap = BitmapFactory.decodeStream(inpStream)
-            } catch (e: Exception) {
-                e.printStackTrace()
-                return null
+            GlobalScope.launch(Dispatchers.Main) {
+                loadBtn.visibility = View.INVISIBLE
+                spinner.visibility = View.VISIBLE
+                bitmap = withContext(Dispatchers.IO) {
+                    BitmapFactory.decodeStream(URL(imgURL).openStream())
+                }
+                bitmap?.run {
+                    spinner.visibility = View.GONE
+                    loadBtn.visibility = View.VISIBLE
+                    loadImg.setImageBitmap(bitmap)
+                }
             }
-
-            return resBitmap
-        }
-
-        override fun onPostExecute(result: Bitmap?) {
-            if (result != null) {
-                loadImg.setImageBitmap(result)
-            }
-            loadBtn.visibility = View.VISIBLE
-            spinner.visibility = View.GONE
         }
     }
 }
