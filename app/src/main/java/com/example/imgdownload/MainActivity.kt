aeb1d3f7df247bf26a085e9e9e5d6f584d024ba4
Leonid Khvatskin <leonkhvatskin@gmail.com>
2020-12-05 03:49:35 +0300

    picasso

--- app/src/main/java/com/example/imgdownload/MainActivity.kt
+++ app/src/main/java/com/example/imgdownload/MainActivity.kt
@@ -1,22 +1,13 @@
 package com.example.imgdownload
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.ProgressBar
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.net.URL
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
 
-    private var bitmap: Bitmap? = null
     private val imgURL = "https://im0-tub-ru.yandex.net/i?id=73b3f62833333e8a1817a3106ac1ef31&n=13"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,22 +15,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val loadImg = findViewById<ImageView>(R.id.load_img)
-        val spinner = findViewById<ProgressBar>(R.id.progress_bar)
         val loadBtn = findViewById<Button>(R.id.load_btn)
 
         loadBtn.setOnClickListener {
-            GlobalScope.launch(Dispatchers.Main) {
-                loadBtn.visibility = View.INVISIBLE
-                spinner.visibility = View.VISIBLE
-                bitmap = withContext(Dispatchers.IO) {
-                    BitmapFactory.decodeStream(URL(imgURL).openStream())
-                }
-                bitmap?.run {
-                    spinner.visibility = View.GONE
-                    loadBtn.visibility = View.VISIBLE
-                    loadImg.setImageBitmap(bitmap)
-                }
-            }
+            Picasso.get().load(imgURL).into(loadImg)
         }
     }
 }
