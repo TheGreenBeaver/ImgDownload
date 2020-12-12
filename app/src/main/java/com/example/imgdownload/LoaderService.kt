24b42cd42cb699cdc5f913e5486ee9ddd3e93761
Leonid Khvatskin <leonkhvatskin@gmail.com>
2020-12-12 12:15:40 +0300

    asd

--- app/src/main/java/com/example/imgdownload/LoaderService.kt
+++ app/src/main/java/com/example/imgdownload/LoaderService.kt
@@ -7,7 +7,11 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
+import android.os.Handler
 import android.os.IBinder
+import android.os.Message
+import android.os.Messenger
+import android.widget.Toast
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
@@ -15,59 +19,80 @@ import java.io.IOException
 
 class LoaderService : Service() {
 
-    private var loadingThread: Thread? = null
+//    private var loadingThread: Thread? = null
+    private lateinit var loaderMessenger: Messenger
 
-    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
-
-        val loadUrl = intent.getStringExtra(Constants.URL_TO_LOAD_KEY.name)
-
-        loadingThread = Thread {
-            if (!Thread.interrupted()) {
-                try {
-                    val inpStream = java.net.URL(loadUrl).openStream()
-                    val resBitmap = BitmapFactory.decodeStream(inpStream)
-
-                    val cw = ContextWrapper(applicationContext)
-                    val dir = cw.getDir("img_download", Context.MODE_PRIVATE)
-                    if (!dir.exists()) {
-                        dir.mkdirs()
-                    }
-                    val pathToFile = "${loadUrl?.replace(Regex("[+=;@#:^&*./%)(><?!-]+"), "_")}.jpg"
-                    val fileToSave = File(dir, pathToFile)
-
-                    val outputStream: FileOutputStream?
-                    try {
-                        outputStream = FileOutputStream(fileToSave, false)
-                        resBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
-                        outputStream.flush()
-                        outputStream.close()
-                    } catch (e: IOException) {
-                        e.printStackTrace()
-                    }
-
-
-                    val resIntent = Intent(Constants.LOADING_SUCCESS_ACTION.name)
-                    resIntent.putExtra(Constants.RES_BITMAP_KEY.name, Uri.fromFile(fileToSave).toString())
-                    sendBroadcast(resIntent)
-                    stopSelf()
-
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                    return@Thread
-                }
+    internal class IncomingHandler(
+        context: Context,
+        private val applicationContext: Context = context.applicationContext
+    ) : Handler() {
+        override fun handleMessage(msg: Message) {
+            when (msg.what) {
+                17 ->
+                    Toast.makeText(applicationContext, "hello! received ${msg.obj}", Toast.LENGTH_SHORT).show()
+                else -> super.handleMessage(msg)
             }
         }
-        loadingThread!!.start()
-
-        return START_REDELIVER_INTENT
     }
 
-    override fun onBind(intent: Intent?): IBinder? {
-        return null
+    override fun onBind(intent: Intent): IBinder? {
+        Toast.makeText(applicationContext, "binding", Toast.LENGTH_SHORT).show()
+        loaderMessenger = Messenger(IncomingHandler(this))
+        return loaderMessenger.binder
     }
 
-    override fun onDestroy() {
-        loadingThread?.interrupt()
-        super.onDestroy()
-    }
+
+
+
+
+
+//    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+//
+//        val loadUrl = intent.getStringExtra(Constants.URL_TO_LOAD_KEY.name)
+//
+//        loadingThread = Thread {
+//            if (!Thread.interrupted()) {
+//                try {
+//                    val inpStream = java.net.URL(loadUrl).openStream()
+//                    val resBitmap = BitmapFactory.decodeStream(inpStream)
+//
+//                    val cw = ContextWrapper(applicationContext)
+//                    val dir = cw.getDir("img_download", Context.MODE_PRIVATE)
+//                    if (!dir.exists()) {
+//                        dir.mkdirs()
+//                    }
+//                    val pathToFile = "${loadUrl?.replace(Regex("[+=;@#:^&*./%)(><?!-]+"), "_")}.jpg"
+//                    val fileToSave = File(dir, pathToFile)
+//
+//                    val outputStream: FileOutputStream?
+//                    try {
+//                        outputStream = FileOutputStream(fileToSave, false)
+//                        resBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+//                        outputStream.flush()
+//                        outputStream.close()
+//                    } catch (e: IOException) {
+//                        e.printStackTrace()
+//                    }
+//
+//
+//                    val resIntent = Intent(Constants.LOADING_SUCCESS_ACTION.name)
+//                    resIntent.putExtra(Constants.RES_BITMAP_KEY.name, Uri.fromFile(fileToSave).toString())
+//                    sendBroadcast(resIntent)
+//                    stopSelf()
+//
+//                } catch (e: Exception) {
+//                    e.printStackTrace()
+//                    return@Thread
+//                }
+//            }
+//        }
+//        loadingThread!!.start()
+//
+//        return START_REDELIVER_INTENT
+//    }
+//
+//    override fun onDestroy() {
+//        loadingThread?.interrupt()
+//        super.onDestroy()
+//    }
 }
--- app/src/main/java/com/example/imgdownload/MainActivity.kt
+++ app/src/main/java/com/example/imgdownload/MainActivity.kt
@@ -18,22 +18,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        urlInput = findViewById(R.id.url_input)
-        loadButton = findViewById(R.id.load_btn)
-
-        loadButton.setOnClickListener {
-            val loadUrl = urlInput.text.toString()
-            if (URLUtil.isValidUrl(loadUrl)) {
-                Intent(this, LoaderService::class.java).also { intent ->
-                    intent.putExtra(Constants.URL_TO_LOAD_KEY.name, loadUrl)
-                    startService(intent)
-                }
-            }
-        }
+//        urlInput = findViewById(R.id.url_input)
+//        loadButton = findViewById(R.id.load_btn)
+//
+//        loadButton.setOnClickListener {
+//            val loadUrl = urlInput.text.toString()
+//            if (URLUtil.isValidUrl(loadUrl)) {
+//                Intent(this, LoaderService::class.java).also { intent ->
+//                    intent.putExtra(Constants.URL_TO_LOAD_KEY.name, loadUrl)
+//                    startService(intent)
+//                }
+//            }
+//        }
     }
 
-    override fun onStop() {
-        stopService(Intent(this, LoaderService::class.java))
-        super.onStop()
-    }
+//    override fun onStop() {
+//        stopService(Intent(this, LoaderService::class.java))
+//        super.onStop()
+//    }
 }
